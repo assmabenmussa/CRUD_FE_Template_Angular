92d2549a7c889f328cdb632d3a40c5f12b926b0e
unknown <assma.bm@gmail.com>
2020-11-28 22:21:47 +0200

    FEATURE: dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      DashboardComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
