9123a939cd61f5f9602c57f62784f92b348a5af0
unknown <assma.bm@gmail.com>
2020-11-28 22:47:06 +0200

    CODE-REFACTOR: moved components into separate files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ViewComponent } from './view/view.component';
-import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ViewComponent } from './components/dashboard/view/view.component';
+import { LoginComponent } from './components/dashboard/login/login.component';
 
 @NgModule({
   declarations: [			
