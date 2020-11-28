808762aab86faa87910f3da1ca490210d536194a
unknown <assma.bm@gmail.com>
2020-11-28 22:47:38 +0200

    BUGFIX: fixed route for app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ViewComponent } from './components/dashboard/view/view.component';
-import { LoginComponent } from './components/dashboard/login/login.component';
+import { ViewComponent } from './components/view/view.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [			
