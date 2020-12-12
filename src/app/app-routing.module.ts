e4ae628164845201047431b0a804ce5b651bee35
unknown <assma.bm@gmail.com>
2020-12-12 22:39:00 +0200

    FEATURE: added login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: "dashboard", pathMatch: "full" ,component: DashboardComponent}
+  {path: "dashboard", pathMatch: "full" , component: DashboardComponent},
+  {path: "login", component: LoginComponent}
 ];
 
 @NgModule({
