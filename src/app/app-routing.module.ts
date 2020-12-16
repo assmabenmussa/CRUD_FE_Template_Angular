c9209e53d01cc987d36c795520adcbe0e83ce3ab
unknown <assma.bm@gmail.com>
2020-12-16 22:35:07 +0200

    FEATURE: added create component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { ViewComponent } from './components/CRUD/view/view.component';
+import { CreateComponent } from './components/CRUD/create/create.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: "dashboard", pathMatch: "full" , component: DashboardComponent},
   {path: "view", component: ViewComponent},
+  {path: "create", component: CreateComponent},
   {path: "login", component: LoginComponent}
 ];
 
