440f1e62f62334f340c17bd465c3a23eac7b5bd9
unknown <assma.bm@gmail.com>
2020-12-16 19:39:29 +0200

    CODE REFACTOR: edited app.module for moving things into CRUD folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ViewComponent } from './components/view/view.component';
+import { ViewComponent } from './components/CRUD/view/view.component';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
