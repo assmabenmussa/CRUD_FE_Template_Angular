1397e486f621359e4c7c3a5983698bf2df82c594
unknown <assma.bm@gmail.com>
2020-12-16 19:25:43 +0200

    FEATURE: child-service

---
+++ src/app/services/child.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MainService } from './main.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class childService extends MainService{
+
+  resource = "child-resource";
+
+}
