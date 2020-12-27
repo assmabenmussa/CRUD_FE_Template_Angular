34fef983f9754367cb2a46c32f261b18af70e222
unknown <assma.bm@gmail.com>
2020-12-27 23:01:01 +0200

    FEATURE: setting up translation in the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,13 @@ import { ViewComponent } from './components/CRUD/view/view.component';
 import { LoginComponent } from './components/login/login.component';
 import { CreateComponent } from './components/CRUD/create/create.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [			
     AppComponent,
@@ -20,7 +26,14 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
