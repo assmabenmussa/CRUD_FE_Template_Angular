21d84ac44531d60245f396d84dbb932991664957
unknown <assma.bm@gmail.com>
2021-01-02 20:13:09 +0200

    FEATURE: initialized localization service

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalizationService } from 'src/app/services/localization.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translate: LocalizationService) { }
 
   ngOnInit() {
   }
