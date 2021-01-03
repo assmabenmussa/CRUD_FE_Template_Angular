8e3f0161e0c89e0d3d3af2cdf761183e35ef596b
unknown <assma.bm@gmail.com>
2021-01-03 09:00:18 +0200

    TRANSLATION: initialized the localization servie

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalizationService } from 'src/app/services/localization.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(translate: LocalizationService) { }
 
   ngOnInit() {
   }
