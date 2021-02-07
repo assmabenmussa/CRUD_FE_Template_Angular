989f5b555c4f1f8e35a503fac264bcc62f660e31
unknown <assma.bm@gmail.com>
2021-02-07 22:46:10 +0200

    LOCALIZATION: embedded localization service into the dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { LocalizationService } from './services/localization.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CRUD-FE-Template-Angular';
+  constructor(public translate: LocalizationService){}
 }
